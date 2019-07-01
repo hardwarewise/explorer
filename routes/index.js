be09781009b8dc56599d5051aa8a5a16d5627dd3
crptec <crptec.fr@gmail.com>
2019-07-01 22:59:28 +0200

    world map
    Signed-off-by: crptec <crptec.fr@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -91,7 +91,7 @@ function route_get_tx(res, txid) {
 }
 
 function route_get_index(res, error) {
-  res.render('index', { active: 'home', error: error, warning: null});
+  res.render('dashboard', { active: 'dashboard', error: error, warning: null});
 }
 
 function route_get_address(res, hash, count) {
